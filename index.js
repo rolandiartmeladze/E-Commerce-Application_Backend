0682e6a8d03d4d80596270414fe43dd69903a9ff
rolandiartmeladze <rartmeladze@gmail.com>
2024-04-04 06:26:40 +0400

    commot ff

--- index.js
+++ index.js
@@ -59,7 +59,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../')));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
 // app.use(express.static(path.join(__dirname, '../')));
 
 
@@ -308,7 +310,7 @@ const linki = basalink + name;
 
 
 
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '../')); });
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '../public/index.html')); });
 
 app.listen(PORT, () => { console.log(`Backend server is running on port ${PORT}`); });
 
