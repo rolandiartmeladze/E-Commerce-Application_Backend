2913f67ff487e9918398a9c98e2a099e257439d7
rolandiartmeladze <rartmeladze@gmail.com>
2024-04-07 17:56:19 +0400

    remove retals

--- functions/example.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/app', (req, res) => {
-  res.json({ message: 'This is an example endpoint!' });
-});
-
-module.exports.handler = app;
